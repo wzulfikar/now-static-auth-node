dd844dbd3fc682b44a0657089264fa98a47f7440
Wildan Zulfikar <me@wzulfikar.com>
2020-05-23 01:39:47 +0800

    Update index.js
    Ensure username and password are not empty

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const AUTH_USERS = process.env.AUTH_USERS
 
 const app = protect(
   '/protected',
-  (user, pass) => AUTH_USERS.includes(`${user}:${pass}`),
+  (user, pass) =>
+    user.trim() && pass.trim() && AUTH_USERS.includes(`${user}:${pass}`),
   { directory: __dirname + '/_static' }
 );
 
